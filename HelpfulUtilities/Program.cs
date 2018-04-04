b93254683f4a9a0484b3f3605cdac3b242ffb5d2
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-04-04 13:11:00 -0500

    Add project files.

---
+++ HelpfulUtilities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelpfulUtilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
