437b4aa47cc86610ecb003f1ab2af004b7467965
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-04-05 15:08:19 -0500

    New package and extensions

---
+++ Discord.NET Utilities/Extensions/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpfulUtilities.Extensions
+{
+    /// <summary>
+    /// This class provides a host of extension methods pertaining to Discord.NET (WebSocket and Rest)
+    /// </summary>
+    public static partial class Extensions { }
+}
