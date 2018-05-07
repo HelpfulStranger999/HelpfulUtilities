44b6463c022f8b121c0d7afcc2ce7e2e3eb1e681
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-07 14:52:38 -0500

    Enum constraint

--- HelpfulUtilities/EnumUtils.cs
+++ HelpfulUtilities/EnumUtils.cs
@@ -13,19 +13,19 @@ namespace HelpfulUtilities
         /// <summary>Returns a random value of the <paramref name="enum"/></summary>
         /// <typeparam name="T">Type of enum</typeparam><param name="enum">The enum</param>
         /// <returns>A random enum value of type <typeparamref name="T"/></returns>
-        public static T Random<T>(Enum @enum) where T : struct, IConvertible
+        public static T Random<T>(Enum @enum) where T : Enum
             => Random<T>(@enum.GetType());
 
         /// <summary>Returns a random value of the <paramref name="obj"/> enum</summary>
         /// <typeparam name="T">Type of enum</typeparam><param name="obj">An object of the enum</param>
         /// <returns>A random enum value of type <typeparamref name="T"/></returns>
-        public static T Random<T>(T obj) where T : struct, IConvertible
+        public static T Random<T>(T obj) where T : Enum
             => Random<T>(typeof(T));
 
         /// <summary>Returns a random value of the <paramref name="type"/> enum</summary>
         /// <typeparam name="T">Type of enum</typeparam><param name="enum">The enum type</param>
         /// <returns>A random enum value of type <typeparamref name="T"/></returns>
-        public static T Random<T>(Type type) where T : struct, IConvertible
+        public static T Random<T>(Type type) where T : Enum
             => Enum.GetValues(type).Cast<T>().Random();
     }
 }
