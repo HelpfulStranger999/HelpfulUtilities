146855f6040e63b5e491b6b4eaee4530d8b966c8
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-09 14:59:15 -0500

    Tests

---
+++ Unit Tests/ListenerServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using Discord.Commands;
+using HelpfulUtilities.Discord.Listeners;
+using HelpfulUtilities.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class ListenerServiceTests
+    {
+
+        [TestMethod]
+        public void EnumerableTest()
+        {
+            IEnumerable<int> Power(int number, int exponent)
+            {
+                int result = 1;
+
+                for (int i = 0; i < exponent; i++)
+                {
+                    result = result * number;
+                    yield return result;
+                }
+            }
+
+            var numbers = Power(2, 8);
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        [TestMethod]
+        public void TestModuleFinder()
+        {
+            try
+            {
+                var service = new ListenerService();
+                var moduleBase = Assembly.GetAssembly(typeof(CommandService)).GetType("Discord.Commands.IModuleBase", true);
+                var list = new List<Type>();
+
+                foreach (var type in Assembly.GetAssembly(typeof(ListenerModuleOne)).GetTypes())
+                {
+                    var result = type.Extends(moduleBase);
+                    if (result)
+                    {
+                        list.Add(type);
+                    }
+
+                }
+                var types = Assembly.GetAssembly(typeof(ListenerModuleOne)).GetTypes().Where(type => type.Extends(moduleBase)).ToList();
+                service.AddModules(Assembly.GetAssembly(typeof(ListenerModuleOne))).ToList();
+                Assert.IsTrue(service.Listeners.Count > 0);
+            }
+            catch (Exception e)
+            {
+                Assert.IsNull(e);
+            }
+        }
+    }
+
+
+    internal class ListenerModuleOne : ModuleBase<MyContext>
+    {
+
+        [Listener]
+        public async Task DoStuff()
+        { }
+    }
+
+    internal class ListenerModuleTwo : ModuleBase<MyContext>
+    {
+        [Listener]
+        public Task DoMoreStuff() => Task.CompletedTask;
+    }
+}
