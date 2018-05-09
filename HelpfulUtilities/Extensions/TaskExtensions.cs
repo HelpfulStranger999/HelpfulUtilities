f2bc9ddfa8b9b2a3e8afeba03afa93aeeda1f291
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-08 19:59:44 -0500

    New Task extensions

--- HelpfulUtilities/Extensions/TaskExtensions.cs
+++ HelpfulUtilities/Extensions/TaskExtensions.cs
@@ -4,10 +4,27 @@ namespace HelpfulUtilities.Extensions
 {
     public static partial class Extensions
     {
-        /// <summary>Runs the task.</summary>
+        /// <summary>
+        /// Runs the task.
+        /// </summary>
         public static void Await(this Task task) => task.GetAwaiter().GetResult();
 
-        /// <summary>Runs the task of type <typeparamref name="T"/> and returns the result.</summary>
+        /// <summary>
+        /// Runs the task of type <typeparamref name="T"/> and returns the result.
+        /// </summary>
         public static T Await<T>(this Task<T> task) => task.GetAwaiter().GetResult();
+
+        /// <summary>
+        /// Runs the task. See <see cref="Task.ConfigureAwait(bool)"/>.
+        /// </summary>
+        public static void Await(this Task task, bool continueOnCapturedContext)
+            => task.ConfigureAwait(continueOnCapturedContext).GetAwaiter().GetResult();
+
+        /// <summary>
+        /// Runs the task of type <typeparamref name="T"/> and returns the result.
+        /// See <see cref="Task.ConfigureAwait(bool)"/>.
+        /// </summary>
+        public static T Await<T>(this Task<T> task, bool continueOnCapturedContext)
+            => task.ConfigureAwait(continueOnCapturedContext).GetAwaiter().GetResult();
     }
 }
