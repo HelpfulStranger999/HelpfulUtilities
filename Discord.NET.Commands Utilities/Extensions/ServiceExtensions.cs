80476d1d88b7c44993aa5fc771420ac39479e971
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-09 15:36:19 -0500

    WIP Smart Search Extension

---
+++ Discord.NET.Commands Utilities/Extensions/ServiceExtensions.cs
@@ -0,0 +1,94 @@
+﻿using Discord.Commands;
+using HelpfulUtilities.Discord.Commands.Results;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpfulUtilities.Discord.Commands.Extensions
+{
+    public static partial class Extensions
+    {
+        internal static async Task<CommandSearchResult> SearchAsync(this CommandService service, ICommandContext context, string input, IServiceProvider services = null)
+        {
+            services = services ?? EmptyServiceProvider.Instance;
+
+            var searchResult = service.Search(context, input);
+            if (!searchResult.IsSuccess) return CommandSearchResult.FromError(null, searchResult);
+
+            var commands = searchResult.Commands;
+            var preconditionResults = new Dictionary<CommandMatch, PreconditionResult>();
+
+            foreach (var match in commands)
+            {
+                preconditionResults[match] = await match.Command.CheckPreconditionsAsync(context, services).ConfigureAwait(false);
+            }
+
+            var successfulPreconditions = preconditionResults
+                .Where(x => x.Value.IsSuccess)
+                .ToArray();
+
+            if (successfulPreconditions.Length == 0)
+            {
+                //All preconditions failed, return the one from the highest priority command
+                var bestCandidate = preconditionResults
+                    .OrderByDescending(x => x.Key.Command.Priority)
+                    .FirstOrDefault(x => !x.Value.IsSuccess);
+                return CommandSearchResult.FromError(bestCandidate.Key, bestCandidate.Value);
+            }
+
+            //If we get this far, at least one precondition was successful.
+
+            var parseResultsDict = new Dictionary<CommandMatch, ParseResult>();
+            foreach (var pair in successfulPreconditions)
+            {
+                var parseResult = await pair.Key.ParseAsync(context, searchResult, pair.Value, services).ConfigureAwait(false);
+
+                if (parseResult.Error == CommandError.MultipleMatches)
+                {
+                    var argList = parseResult.ArgValues.Select(x => x.Values.OrderByDescending(y => y.Score).First()).ToImmutableArray();
+                    var paramList = parseResult.ParamValues.Select(x => x.Values.OrderByDescending(y => y.Score).First()).ToImmutableArray();
+                    parseResult = ParseResult.FromSuccess(argList, paramList);
+                }
+
+                parseResultsDict[pair.Key] = parseResult;
+            }
+
+            // Calculates the 'score' of a command given a parse result
+            float CalculateScore(CommandMatch match, ParseResult parseResult)
+            {
+                var totalScore = 0f;
+                if (match.Command.Parameters.Count > 0)
+                {
+                    var argScore = parseResult.ArgValues?.Sum(x => x.Values.OrderByDescending(y => y.Score).FirstOrDefault().Score) ?? 0;
+                    var paramScore = parseResult.ParamValues?.Sum(x => x.Values.OrderByDescending(y => y.Score).FirstOrDefault().Score) ?? 0;
+                    totalScore = (argScore + paramScore) / (2 * match.Command.Parameters.Count);
+                }
+
+                return match.Command.Priority + totalScore * 0.99f;
+            }
+
+            //Order the parse results by their score so that we choose the most likely result to execute
+            var parseResults = parseResultsDict
+                .OrderByDescending(x => CalculateScore(x.Key, x.Value));
+
+            var successfulParses = parseResults
+                .Where(x => x.Value.IsSuccess)
+                .ToArray();
+
+            if (successfulParses.Length == 0)
+            {
+                //All parses failed, return the one from the highest priority command, using score as a tie breaker
+                var bestMatch = parseResults
+                    .FirstOrDefault(x => !x.Value.IsSuccess);
+                return CommandSearchResult.FromError(bestMatch.Key, bestMatch.Value);
+            }
+
+            //If we get this far, at least one parse was successful. Execute the most likely overload.
+            var chosenOverload = successfulParses[0];
+            return CommandSearchResult.FromSuccess(chosenOverload.Key);
+        }
+    }
+}
---
+++ Discord.NET.Commands Utilities/Results/CommandSearchResult.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System.Collections.Generic;
+
+namespace HelpfulUtilities.Discord.Commands.Results
+{
+    internal class CommandSearchResult
+    {
+        public CommandMatch BestMatch { get; private set; }
+        public IResult Error { get; private set; }
+        public bool IsSuccess => Error == null;
+
+        private CommandSearchResult(CommandMatch match, IResult result = null)
+        {
+            BestMatch = match;
+            Error = result;
+        }
+
+        public static CommandSearchResult FromSuccess(CommandMatch match)
+            => new CommandSearchResult(match, null);
+        public static CommandSearchResult FromError(CommandMatch? bestMatch, IResult result)
+            => new CommandSearchResult(bestMatch.GetValueOrDefault(default(CommandMatch)), result);
+    }
+}
