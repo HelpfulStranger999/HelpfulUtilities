006fd64312e80c8b4bf684ece3dff2711a729654
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-09 15:36:06 -0500

    Empty service provider

---
+++ Discord.NET.Commands Utilities/EmptyServiceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelpfulUtilities.Discord.Commands
+{
+    /// <summary>Represents a <see cref="IServiceProvider"/> that is empty.</summary>
+    public class EmptyServiceProvider : IServiceProvider
+    {
+        /// <summary>Provides a <see langword="static"/> <see cref="EmptyServiceProvider"/></summary>
+        public static readonly EmptyServiceProvider Instance = new EmptyServiceProvider();
+        /// <inheritdoc />
+        public object GetService(Type serviceType) => null;
+    }
+}
