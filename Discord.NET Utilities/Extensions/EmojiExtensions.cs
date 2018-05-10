ca7429aa84751342385c4ec10a758c04bdc4a0e6
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-10 08:56:56 -0500

    Fixes emoji parser

--- Discord.NET Utilities/Extensions/EmojiExtensions.cs
+++ Discord.NET Utilities/Extensions/EmojiExtensions.cs
@@ -1,11 +1,16 @@
 ﻿using Discord;
+#if EMOJI
+using Discord.Addons.EmojiTools;
+#endif
 
 namespace HelpfulUtilities.Discord.Extensions
 {
     public static partial class Extensions
     {
+#if EMOJI
         /// <summary>Returns the url for this emoji.</summary>
         public static string GetUrl(this Emoji emoji)
-            => $"https://raw.githubusercontent.com/HenrikJoreteg/emoji-images/master/pngs/{emoji.Name}.png";
+            => $"https://raw.githubusercontent.com/HenrikJoreteg/emoji-images/master/pngs/{emoji.GetShorthand()}.png";
+#endif
     }
 }
