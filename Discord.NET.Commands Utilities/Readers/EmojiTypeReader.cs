9b486e58b41c5b2b9743b6d2803efb3ce1a25cf6
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-10 08:56:40 -0500

    Fixes emoji reader

--- Discord.NET.Commands Utilities/Readers/EmojiTypeReader.cs
+++ Discord.NET.Commands Utilities/Readers/EmojiTypeReader.cs
@@ -1,9 +1,8 @@
+#if EMOJI
 using Discord;
+using Discord.Addons.EmojiTools;
 using Discord.Commands;
-using HelpfulUtilities.Discord.Commands.Properties;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,85 +12,14 @@ namespace HelpfulUtilities.Discord.Commands.Readers
     /// <summary>This <see cref="TypeReader"/> parses emoji.</summary>
     public class EmojiTypeReader : TypeReader
     {
-        /// <summary>A dictionary containing all emojis with the name as the key and the emoji as value</summary>
-        public static IReadOnlyDictionary<string, string> EmojiSet { get; }
-
-        static EmojiTypeReader()
-        {
-            var emojis = JsonConvert.DeserializeObject<EmojiLoader>(Resources.Emoji);
-            EmojiSet = emojis.Generate();
-        }
 
         /// <summary>Parses the emoji</summary>
         public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
         {
-            if (EmojiSet.Values.Contains(input.Trim()))
+            if (EmojiMap.Map.Values.Contains(input.Trim(':')))
                 return Task.FromResult(TypeReaderResult.FromSuccess(new Emoji(input)));
             return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Input was not an emoji"));
         }
     }
-    
-    internal class EmojiLoader
-    {
-        [JsonProperty("people")]
-        public EmojiSet[] People { get; set; }
-        [JsonProperty("nature")]
-        public EmojiSet[] Nature { get; set; }
-        [JsonProperty("food")]
-        public EmojiSet[] Food { get; set; }
-        [JsonProperty("activity")]
-        public EmojiSet[] Activity { get; set; }
-        [JsonProperty("travel")]
-        public EmojiSet[] Travel { get; set; }
-        [JsonProperty("objects")]
-        public EmojiSet[] Objects { get; set; }
-        [JsonProperty("symbols")]
-        public EmojiSet[] Symbols { get; set; }
-        [JsonProperty("flags")]
-        public EmojiSet[] Flags { get; set; }
-
-        public IReadOnlyDictionary<string, string> Generate()
-        {
-            var dict = new Dictionary<string, string>();
-
-            foreach (var set in People)
-                set.Generate(ref dict);
-            foreach (var set in Nature)
-                set.Generate(ref dict);
-            foreach (var set in Food)
-                set.Generate(ref dict);
-            foreach (var set in Activity)
-                set.Generate(ref dict);
-            foreach (var set in Travel)
-                set.Generate(ref dict);
-            foreach (var set in Objects)
-                set.Generate(ref dict);
-            foreach (var set in Symbols)
-                set.Generate(ref dict);
-            foreach (var set in Flags)
-                set.Generate(ref dict);
-
-            return dict.ToImmutableDictionary();
-        }
-
-        internal class EmojiSet
-        {
-            [JsonProperty("names")]
-            public string[] Names { get; set; }
-
-            [JsonProperty("surrogates")]
-            public string Emoji { get; set; }
-
-            [JsonProperty("hasDiversity", Required = Required.Default)]
-            public bool Diversity { get; set; } = false;
-
-            public void Generate(ref Dictionary<string, string> dict)
-            {
-                foreach (var name in Names)
-                {
-                    dict.Add(name, Emoji);
-                }
-            }
-        }
-    }
 }
+#endif
--- Discord.NET.Commands Utilities/Readers/IEmoteTypeReader.cs
+++ Discord.NET.Commands Utilities/Readers/IEmoteTypeReader.cs
@@ -8,19 +8,32 @@ namespace HelpfulUtilities.Discord.Commands.Readers
     /// <summary>This <see cref="TypeReader"/> parses <see cref="IEmote"/>.</summary>
     public class IEmoteTypeReader : TypeReader
     {
+#if EMOJI
         private EmojiTypeReader Emoji = new EmojiTypeReader();
+#endif
 
         /// <summary>Parses the <see cref="IEmote"/></summary>
+#if EMOJI
         public async override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
         {
             var result = await Emoji.ReadAsync(context, input, services);
-            if (result.IsSuccess)
-                return result;
+            if (result.IsSuccess) return result;
 
             if (Emote.TryParse(input, out var emote))
                 return TypeReaderResult.FromSuccess(emote);
 
             return TypeReaderResult.FromError(CommandError.ParseFailed, "IEmote could not be parsed from input.");
         }
+#else
+        public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
+        {
+            if (Emote.TryParse(input, out var emote))
+                return Task.FromResult(TypeReaderResult.FromSuccess(emote));
+
+            return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed,
+                "IEmote could not be parsed from input."));
+        }
+#endif
+
     }
 }
