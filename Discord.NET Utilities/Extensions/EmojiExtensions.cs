b1b47d9cbb086bfc3dc3a7ade3289af879a561b7
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-10 10:03:41 -0500

    Simpler reworking

--- Discord.NET Utilities/Extensions/EmojiExtensions.cs
+++ Discord.NET Utilities/Extensions/EmojiExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using System;
 #if EMOJI
 using Discord.Addons.EmojiTools;
 #endif
@@ -7,10 +8,12 @@ namespace HelpfulUtilities.Discord.Extensions
 {
     public static partial class Extensions
     {
-#if EMOJI
         /// <summary>Returns the url for this emoji.</summary>
+        /// <remarks>Original (if simple) source at <see href="https://github.com/discord-csharp/MODiX/blob/master/Modix/Modules/FunModule.cs#L29-L32">this GitHub project.</see></remarks>
         public static string GetUrl(this Emoji emoji)
-            => $"https://raw.githubusercontent.com/HenrikJoreteg/emoji-images/master/pngs/{emoji.GetShorthand()}.png";
-#endif
+        {
+            var codepoint = Char.ConvertToUtf32(emoji.Name, 0).ToString("X").ToLower();
+            return $"https://raw.githubusercontent.com/twitter/twemoji/gh-pages/2/72x72/{codepoint}.png"; ;
+        }
     }
 }
