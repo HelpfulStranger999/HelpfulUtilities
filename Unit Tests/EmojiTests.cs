051efdbfb17d6263b2c156a463065387d92a9008
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-10 10:08:55 -0500

    unit test

---
+++ Unit Tests/EmojiTests.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelpfulUtilities.Discord.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HelpfulUtilities.Extensions;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class EmojiTests
+    {
+        [TestMethod]
+        public void HasEmojiExtension()
+        {
+            var emoji = new Emoji("😃");
+            var url = emoji.GetUrl();
+        }
+    }
+}
