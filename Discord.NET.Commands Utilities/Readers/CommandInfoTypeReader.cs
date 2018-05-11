9756b224e07b690d6d515d81f9c1486326b3d890
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-11 11:38:13 -0500

    CommandInfo Type Reader

---
+++ Discord.NET.Commands Utilities/Readers/CommandInfoTypeReader.cs
@@ -0,0 +1,36 @@
+﻿using Discord.Commands;
+using HelpfulUtilities.Discord.Commands.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpfulUtilities.Discord.Commands.Readers
+{
+    /// <summary>This <see cref="TypeReader"/> parses <see cref="CommandInfo"/>.</summary>
+    public class CommandInfoTypeReader : TypeReader
+    {
+        /// <summary>Provides a default static error message</summary>
+        public static TypeReaderResult Error = TypeReaderResult.FromError(CommandError.ObjectNotFound, "Could not find any commands matching the given input.");
+
+        /// <summary>Parses the command</summary>
+        public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
+        {
+            var search = services.GetService<CommandService>().Search(context, input);
+            if (!search.IsSuccess) return Task.FromResult(Error);
+
+            var matches = search.Commands.Where(cmd =>
+            {
+                return !cmd.Command.IsHidden();
+            });
+
+            if (matches.Count() <= 0) return Task.FromResult(Error);
+
+            var first = matches.FirstOrDefault();
+            if (first.Equals(default)) return Task.FromResult(Error);
+            return Task.FromResult(TypeReaderResult.FromSuccess(first));
+        }
+    }
+}
