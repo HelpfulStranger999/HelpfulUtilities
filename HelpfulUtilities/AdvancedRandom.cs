57cd0fdee6a58bd95e44c2d31835ad17e93c2496
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-18 14:52:05 -0500

    Major rework of random generator

--- HelpfulUtilities/AdvancedRandom.cs
+++ HelpfulUtilities/AdvancedRandom.cs
@@ -1,107 +1,168 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 
 namespace HelpfulUtilities
 {
     /// <summary>
-    /// Represents an advanced random number generator
+    /// Represents a random number generator that extends upon <see cref="Random"/>
     /// </summary>
-    public class AdvancedRandom
+    public class AdvancedRandom : Random
     {
         /// <summary>
-        /// Whether this generates numbers securely
+        /// Creates an instance of <see cref="AdvancedRandom"/>
         /// </summary>
-        public bool IsSecure = false;
-
-        /// <summary>
-        /// The insecure random number generator to use.
-        /// </summary>
-        protected Random RNG;
-
-        /// <summary>
-        /// The secure random number generator to use.
-        /// </summary>
-        protected RandomNumberGenerator SecureRNG;
+        public AdvancedRandom() : base() { }
 
         /// <summary>
         /// Creates an instance of <see cref="AdvancedRandom"/>
         /// </summary>
-        /// <param name="secure">Whether this RNG should be secure.</param>
-        /// <param name="seed">The seed that should be used for the random number generator if insecure.</param>
-        public AdvancedRandom(bool secure = false, int seed = -1)
-        {
-            IsSecure = secure;
-            if (secure)
-            {
-                SecureRNG = RandomNumberGenerator.Create();
-                RNG = null;
-            }
-            else
-            {
-                SecureRNG = null;
-                RNG = new Random(seed == -1 ? Environment.TickCount : seed);
-            }
-        }
+        /// <param name="seed">The seed that should be used in generating random numbers</param>
+        public AdvancedRandom(int seed) : base(seed) { }
 
         /// <summary>
         /// Generates a random <see cref="bool"/>
         /// </summary>
         /// <returns>The random boolean</returns>
-        public bool Next() => BitConverter.ToBoolean(NextInternal(0, 1), 0);
-
-        /// <summary>Generates a random <see cref="int"/></summary>
-        /// <param name="min">The minimum bound</param><param name="max">The maximum bound</param>
-        /// <returns>The random int</returns>
-        public int Next(int min = 0, int max = 1) => BitConverter.ToInt32(NextInternal(min, max), 0) % (max - min + 1) + min;
-        
-        /// <summary>Generates a random <see cref="double"/></summary>
-        /// <param name="min">The minimum bound</param> <param name="max">The maximum bound</param>
-        /// <returns>The random double</returns>
-        public double Next(double min = 0, double max = 1) => BitConverter.ToDouble(NextInternal(min, max), 0) % (max - min + 1) + min;
-
-        /// <summary>Generates a random <see cref="float"/></summary>
-        /// <param name="min">The minimum bound</param> <param name="max">The maximum bound</param>
-        /// <returns>The random float</returns>
-        public float Next(float min = 0, float max = 1) => BitConverter.ToSingle(NextInternal(min, max), 0) % (max - min + 1) + min;
-
-
-        /// <summary>Generates a random <see cref="long"/></summary>
-        /// <param name="min">The minimum bound</param> <param name="max">The maximum bound</param>
-        /// <returns>The random long</returns>
-        public long Next(long min = 0, long max = 1) => BitConverter.ToInt64(NextInternal(min, max), 0) % (max - min + 1) + min;
-
-        /// <summary>Generates a random <see cref="uint"/></summary>
-        /// <param name="min">The minimum bound</param> <param name="max">The maximum bound</param>
-        /// <returns>The random uint</returns>
-        public uint Next(uint min = 0, uint max = 1) => BitConverter.ToUInt32(NextInternal(min, max), 0) % (max - min + 1) + min;
-
-        /// <summary>Generates a random <see cref="ulong"/></summary>
-        /// <param name="min">The minimum bound</param> <param name="max">The maximum bound</param>
-        /// <returns>The random ulong</returns>
-        public ulong Next(ulong min = 0, ulong max = 1) => BitConverter.ToUInt64(NextInternal(min, max), 0) % (min - max);
-
-        private byte[] NextInternal<T>(T min, T max)
-            where T : IComparable, IFormattable, IConvertible
+        public bool NextBool()
         {
-            Test(min, max);
-            var bytes = new byte[Marshal.SizeOf(typeof(T))];
+            var bytes = new byte[sizeof(bool)];
+            NextBytes(bytes);
+            return BitConverter.ToBoolean(bytes, 0);
+        }
 
-            if (IsSecure)
-                SecureRNG.GetBytes(bytes);
-            else
-                RNG.NextBytes(bytes);
-
+        /// <summary>
+        /// Generates a byte array of the specified size filled with random bytes.
+        /// </summary>
+        /// <param name="size">The size of the byte array</param>
+        /// <returns>The randomly filled byte array</returns>
+        public byte[] NextBytes(int size)
+        {
+            var bytes = new byte[size];
+            NextBytes(bytes);
             return bytes;
         }
 
-        private void Test<T>(T min, T max) where T : IComparable
+        #region Byte
+
+        /// <summary>Generates a random <see cref="sbyte"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random signed byte</returns>
+        public sbyte Next(sbyte min = 0, sbyte max = 1)
         {
-            if (min.CompareTo(max) == 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(min),
-                    "The minimum value exceeded the maximum value");
-            }
+            ErrorTests.MaxMinCheck(max, min, nameof(min));
+            return (sbyte)Next((byte)min, (byte)max);
         }
+
+        /// <summary>Generates a random <see cref="byte"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random byte</returns>
+        public byte Next(byte min = 0, byte max = 1)
+        {
+            ErrorTests.MaxMinCheck(max, min);
+            return (byte)(NextBytes(sizeof(byte))[0]
+                % (max - min + 1) + min);
+        }
+
+        #endregion
+
+        #region Short
+
+        /// <summary>Generates a random <see cref="short"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random short</returns>
+        public short Next(short min = 0, short max = 1)
+        {
+            ErrorTests.MaxMinCheck(max, min, nameof(min));
+            return (short)Next((ushort)min, (ushort)max);
+        }
+
+        /// <summary>Generates a random <see cref="ushort"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random ushort</returns>
+        public ushort Next(ushort min = 0, ushort max = 1)
+        {
+            ErrorTests.MaxMinCheck(max, min);
+            return (ushort)(BitConverter.ToUInt16(NextBytes(sizeof(ushort)), 0)
+                % (max - min + 1) + min);
+        }
+
+        #endregion
+
+        #region Integer
+
+        /// <summary>Generates a random <see cref="uint"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random uint</returns>
+        public uint Next(uint min = 0, uint max = 1)
+        {
+            ErrorTests.MaxMinCheck(max, min);
+            return BitConverter.ToUInt32(NextBytes(sizeof(uint)), 0)
+                % (max - min + 1) + min;
+        }
+
+        #endregion
+
+        #region Long
+
+        /// <summary>Generates a random <see cref="long"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random long</returns>
+        public long Next(long min = 0, long max = 1)
+        {
+            ErrorTests.MaxMinCheck(max, min, nameof(min));
+            return (long)Next((ulong)min, (ulong)max);
+        }
+
+        /// <summary>Generates a random <see cref="ulong"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random ulong</returns>
+        public ulong Next(ulong min = 0, ulong max = 1)
+        {
+            ErrorTests.MaxMinCheck(min, max, nameof(min));
+            return BitConverter.ToUInt64(NextBytes(sizeof(ulong)), 0)
+                % (max - min + 1) + min;
+        }
+
+        #endregion
+
+        #region Floating-Point
+
+        /// <summary>Generates a random <see cref="decimal"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random decimal</returns>
+        public decimal Next(decimal min = 0, decimal max = 1)
+        {
+            int GenInt() => BitConverter.ToInt32(NextBytes(sizeof(int)), 0);
+            return new decimal(GenInt(), GenInt(), GenInt(), NextBool(), Next((byte)0, 28));
+        }
+
+        /// <summary>Generates a random <see cref="double"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random double</returns>
+        public double Next(double min = 0, double max = 1)
+        {
+            ErrorTests.MaxMinCheck(max, min, nameof(min));
+            return (NextDouble() * (max - min)) + min;
+        }
+
+        /// <summary>Generates a random <see cref="float"/></summary>
+        /// <param name="min">The minimum bound</param>
+        /// <param name="max">The maximum bound</param>
+        /// <returns>The random float</returns>
+        public float Next(float min = 0, float max = 1)
+        {
+            ErrorTests.MaxMinCheck(max, min, nameof(min));
+            return (float)Next((double)min, max);
+        }
+
+        #endregion
     }
 }
--- HelpfulUtilities/ErrorTests.cs
+++ HelpfulUtilities/ErrorTests.cs
@@ -21,5 +21,170 @@ namespace HelpfulUtilities
                 throw new ArgumentNullException();
             }
         }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum integer</param>
+        /// <param name="min">The minimum integer</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(int max, int min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum long</param>
+        /// <param name="min">The minimum long</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(long max, long min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum short</param>
+        /// <param name="min">The minimum short</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(short max, short min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum unsigned integer</param>
+        /// <param name="min">The minimum unsigned integer</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(uint max, uint min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum unsigned long</param>
+        /// <param name="min">The minimum unsigned long</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(ulong max, ulong min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum unsigned short</param>
+        /// <param name="min">The minimum unsigned short</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(ushort max, ushort min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum byte</param>
+        /// <param name="min">The minimum byte</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(byte max, byte min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum signed byte</param>
+        /// <param name="min">The minimum signed byte</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(sbyte max, sbyte min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum floating-point value</param>
+        /// <param name="min">The minimum floating-point value</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(float max, float min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum double</param>
+        /// <param name="min">The minimum double</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(double max, double min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
+
+        /// <summary>
+        /// This method tests for <paramref name="min"/> being greater than <paramref name="max"/>
+        /// </summary>
+        /// <param name="max">The maximum decimal</param>
+        /// <param name="min">The minimum decimal</param>
+        /// <param name="name">The name of the parameter for throwing <see cref="ArgumentOutOfRangeException"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="min"/> is greater than <paramref name="max"/></exception>
+        public static void MaxMinCheck(decimal max, decimal min, string name = null)
+        {
+            if (min < max)
+            {
+                throw new ArgumentOutOfRangeException(name ?? nameof(min));
+            }
+        }
     }
 }
