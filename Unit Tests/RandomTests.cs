2456f38b6c02539303104bbe3aa5a3ad961dedc5
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-18 14:52:15 -0500

    Partial tests

---
+++ Unit Tests/RandomTests.cs
@@ -0,0 +1,20 @@
+﻿using HelpfulUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class RandomTests
+    {
+        public AdvancedRandom Random { get; } = new AdvancedRandom(false);
+
+        [TestMethod]
+        public void TestBool()
+        {
+
+        }
+    }
+}
