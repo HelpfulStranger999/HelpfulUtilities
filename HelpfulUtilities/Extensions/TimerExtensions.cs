37a51ef3884a1bee70c7f58f9f224ad065b65ea4
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-05-23 16:20:31 -0500

    Timer extensions

---
+++ HelpfulUtilities/Extensions/TimerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace HelpfulUtilities.Extensions
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Cancels the timer from running anymore
+        /// </summary>
+        /// <remarks>
+        /// Alias for <see cref="Timer.Change(int, int)"/> with <see cref="Timeout.Infinite"/>
+        /// </remarks>
+        public static void Cancel(this Timer timer)
+            => timer.Change(Timeout.Infinite, Timeout.Infinite);
+    }
+}
