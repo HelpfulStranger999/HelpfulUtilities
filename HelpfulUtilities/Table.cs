4f1027a15831d1910be7c9feaa7df1a113342764
HelpfulStranger999 <helpfulstranger999@gmail.com>
2018-10-15 17:26:53 -0500

    Start to update, refamiliarize myself with codebase

---
+++ HelpfulUtilities/Table.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpfulUtilities
+{
+    public class Table<R, C, V> : ICollection<DualKeyPair<R, C>
+    {
+    }
+
+    public struct DualKeyPair<R, C>
+    {
+        public R Row { get; }
+        public C Column { get; }
+
+        public DualKeyPair(R row, C column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Row.GetHashCode() ^ Column.GetHashCode();
+        }
+    }
+}
